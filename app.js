9bf2d0fb25bdfaee240533749fa5c3fd5eb2df2d
flxw <felix@flxw.de>
2013-08-18 14:58:10 +0200

    Setup base for building authentication service.
    This commits enables checking for a logged-in user.
    How the user logs in, or logs out, or changes his password,
    or even registers, still needs to be done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var express = require('express'),
-	path = require('path'),
-	exercises = require('./routes/exercises')
+var express    = require('express');
+var path       = require('path');
+var exercises  = require('./modules/exercises');
+var accounting = require('./modules/accounting');
 
 var app = express()
 
@@ -12,9 +13,31 @@ app.configure(function() {
 
 app.get('/api/exercises', exercises.findAll)
 app.get('/api/exercises/:id', exercises.findById)
-app.post('/api/exercises', exercises.add)
-app.put('/api/exercises/:id', exercises.update)
-app.delete('/api/exercises/:id', exercises.delete)
+
+// These three requests require authentication
+app.post('/api/exercises', function(req, res) {
+    if (accounting.isAuthorized(req,res)) {
+        exercises.add(req,res);
+    } else {
+        // somehow deny access - should be coordinated with UI development
+    }
+});
+
+app.put('/api/exercises/:id', function(req,res) {
+    if (accounting.isAuthorized(req,res)) {
+        exercises.update(req,res);
+    } else {
+        // somehow deny access - should be coordinated with UI development
+    }
+});
+
+app.delete('/api/exercises/:id', function(req,res) {
+    if (accounting.isAuthorized(req,res)) {
+        exercises.delete(req,res);
+    } else {
+        // somehow deny access - should be coordinated with UI development
+    }
+});
 
 app.listen(3000)
 console.log('Listening on port 3000...')
