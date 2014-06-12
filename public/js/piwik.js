079653efbffb8bc1167e8157babc5c2274ad85e8
adius <mail@adriansieber.com>
2014-06-12 08:59:46 +0200

    Removed obsolete libraries

--- public/js/piwik.js
+++ public/js/piwik.js
@@ -1,6 +1,6 @@
 var pkBaseURL = (("https:" == document.location.protocol) ?
-	"https://piwik.adriansieber.com/" :
-	"http://piwik.adriansieber.com/")
+                 "https://piwik.adriansieber.com/" :
+                 "http://piwik.adriansieber.com/")
 
 document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"))
 
@@ -8,5 +8,6 @@ try {
 	var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 2)
 	piwikTracker.trackPageView()
 	piwikTracker.enableLinkTracking()
-} catch(err) {
+}
+catch (err) {
 }
