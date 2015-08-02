a9b35b48a36a122b709111d22e7dedabf74f9105
Adrian Sieber <mail@adriansieber.com>
2015-08-02 11:39:56 +0200

    Improve rendering of single lesson
    - Use commonmark instead of marked
    - Use yaml file to store lesson infos
    - Create custom image element with srcset property and no parent <p>

--- api/lessons.js
+++ api/lessons.js
@@ -3,21 +3,17 @@
 require('es6-promise').polyfill()
 
 var path = require('path'),
-	marked = require('marked'),
 	fsp = require('fs-promise'),
 	yaml = require('js-yaml'),
+	commonmark = require('commonmark'),
 
 	exportObject = {},
 	knowledgeBasePath = path.resolve(
 		__dirname, '../node_modules/knowledge_base'
 	),
-	lessonsPath = path.join(knowledgeBasePath, 'lessons')
-
-
-marked.setOptions({
-	breaks: false,
-	sanitize: true
-})
+	lessonsPath = path.join(knowledgeBasePath, 'lessons'),
+	reader = new commonmark.Parser(),
+	writer = new commonmark.HtmlRenderer()
 
 
 exportObject.getAll = function () {
@@ -26,28 +22,34 @@ exportObject.getAll = function () {
 		.readdir(lessonsPath)
 		.then(function (lessons) {
 
-			var lessonsPromises = lessons
-				.map(function (lesson) {
-					return fsp
-						.readFile(path.resolve(
-							lessonsPath, lesson, 'index.md'
-						))
-						.then(function (fileContent) {
-							return {
-								id: lesson,
-								title: lesson,
-								summary: ''
-								//fileContent.toString().substr(0, 100)
-							}
-						})
-						.catch(function (error) {
-							if (error.code !== 'ENOTDIR')
-								throw error
-						})
+			var lessonsPromises = lessons .map(function (lesson) {
+				return fsp
+					.readFile(path.resolve(
+						lessonsPath, lesson, 'index.yaml'
+					))
+					.catch(function (error) {
+						if (error.code !== 'ENOENT')
+							throw error
+					})
+					.then(function (fileContent) {
+						var descriptionObject = {}
+
+						if (fileContent)
+							descriptionObject = yaml.safeLoad(fileContent)
+
+						descriptionObject.id = lesson
+						descriptionObject.title = descriptionObject.title ||
+							lesson
+
+						return descriptionObject
+					})
+					.catch(function (error) {
+						if (error.code !== 'ENOTDIR')
+							throw error
+					})
 			})
 
-			return Promise
-				.all(lessonsPromises)
+			return Promise.all(lessonsPromises)
 		})
 		.catch(function (error) {
 			if (error.code !== 'ENOENT')
@@ -56,6 +58,30 @@ exportObject.getAll = function () {
 }
 
 exportObject.getById = function (id) {
+
+	function improveImageNode (nodeEvent) {
+
+		var node = nodeEvent.node,
+			imageNode
+
+		if (node.type === 'Paragraph' &&
+			!nodeEvent.entering &&
+			node.firstChild === node.lastChild &&
+			node.firstChild.type === 'Image'
+		) {
+				imageNode = new commonmark.Node('HtmlBlock')
+				imageNode.literal = '<img srcset="' +
+					id + '/' + node.firstChild.destination + ' 1x, ' +
+					id + '/' +
+					node.firstChild.destination.replace('.', '_hq.') + ' 2x' +
+					'" alt="' +
+					node.firstChild.literal +
+					'">'
+
+				node.insertAfter(imageNode)
+				node.unlink()
+			}
+	}
 
 	return Promise
 		.all([
@@ -66,13 +92,19 @@ exportObject.getById = function (id) {
 			var descriptionObject = yaml.safeLoad(array[0]),
 				contentString = array[1].toString(),
 				numberOfWords = contentString.split(' ').length,
-				wordsPerMinute = 90
+				wordsPerMinute = 90,
+				parsedMarkdown = reader.parse(contentString),
+				walker = parsedMarkdown.walker(),
+				nodeEvent
+
+			while (nodeEvent = walker.next())
+				improveImageNode(nodeEvent)
 
 			descriptionObject.numberOfWords = numberOfWords
 			descriptionObject.readingDuration = Math.round(
 				numberOfWords / wordsPerMinute
 			)
-			descriptionObject.content = marked(contentString)
+			descriptionObject.content = writer.render(parsedMarkdown)
 
 			return descriptionObject
 		})
