286a2b866e69eb5c865b43a3e27d5af5bb908eff
Adrian Sieber <mail@adriansieber.com>
2025-10-26 16:01:41 +0000

    Manage dependencies with Nix Flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bash
+            bun
+            coreutils
+            gnumake
+          ];
+        };
+        formatter = pkgs.nixfmt-tree; # Format this file with `nix fmt`
+      }
+    );
+}
